ee25949f29935a4c0abe9ff350a526161d6215b5
JADI-SOUJITH <jadisoujith@JADIs-MacBook-Air.local>
2026-05-12 13:53:26 +0530

    remove google import

--- collector.py
+++ collector.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from google import genai
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 from groq import Groq
